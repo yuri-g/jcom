92661faf1a887786a10e7b234eb1a2c5e9b849a2
Yuri g <yuri.goncharenko@gmail.com>
2013-06-06 14:00:29 +0200

    initial commit

---
+++ src/main/java/im/yuri/jcom/App.java
@@ -0,0 +1,13 @@
+package im.yuri.jcom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
